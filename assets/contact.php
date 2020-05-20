bf2feea3078a1b5ac6409aa2611ee0aaf6cc90c7
Lambda_School_Loaner_282 <lambda_school_loaner_282@Lambda-School-Loaner-282s-MacBook-Pro.local>
2020-05-19 21:14:33 -0500

    working more on contact

--- assets/contact.php
+++ assets/contact.php
@@ -1,7 +1,7 @@
 <?php
      $name = $_POST['name'];
 	 $email = $_POST['email'];
-	 
+	 $topic = $_POST['subject'];
      $message = $_POST['message']; 
 
 
@@ -10,7 +10,7 @@ $subject = 'Message from a site visitor '.$name;
 
 $body_message = 'From: '.$name."\n";
 $body_message .= 'E-mail: '.$email."\n";
-$body_message .= 'Subject: '.$subject."\n";
+$body_message .= 'Subject: '.$topic."\n";
 $body_message .= 'Message: '.$message;
 
 $headers = 'From: '.$email."\r\n";
@@ -21,14 +21,14 @@ $mail_status = mail($mail_to, $subject, $message, $headers);
 if ($mail_status) { ?>
 	<script language="javascript" type="text/javascript">
 		alert('Thank you for the message. We will contact you shortly.');
-		window.location = 'contact.html';
+		window.location = 'index.html';
 	</script>
 <?php
 }
 else { ?>
 	<script language="javascript" type="text/javascript">
 		alert('Message failed. Please, send an email to host@theresamlennon.com');
-		window.location = 'contact_page.html';
+		window.location = 'index.html';
 	</script>
 <?php
 }
